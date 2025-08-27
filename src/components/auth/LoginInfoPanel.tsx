e4de565103ed9a2f43ee1e2e05cca1e48144c691
Haikal Akbar <haikalakbar4554@gmail.com>
2025-08-27 02:28:53 +0000

    tidak jadi kemlakikan sepeeti pertama tadi

--- src/components/auth/LoginInfoPanel.tsx
+++ src/components/auth/LoginInfoPanel.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
@@ -6,6 +5,8 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 export function LoginInfoPanel() {
   return (
     <div className="relative hidden w-full overflow-hidden bg-primary p-8 text-white md:flex md:w-1/2 flex-col justify-center items-center">
+        <div className="absolute -top-16 -left-16 w-64 h-64 bg-primary/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+        <div className="absolute -bottom-24 -right-2 w-72 h-72 bg-accent/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         <div className="w-full max-w-md">
             <DotLottieReact
               src="https://lottie.host/a94356f3-ad72-4a9d-a141-edd697b2bc69/BKMQRlHksC.lottie"
