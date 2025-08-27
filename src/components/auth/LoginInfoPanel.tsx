d57b3d58b69750125cd27f10aa9d97c0cc6232ab
Haikal Akbar <haikalakbar4554@gmail.com>
2025-08-27 02:57:43 +0000

    tambhakan juga logo kemenag di pojok kiri atas di atsa foto ketuakua.png

--- src/components/auth/LoginInfoPanel.tsx
+++ src/components/auth/LoginInfoPanel.tsx
@@ -6,14 +6,22 @@ import Image from 'next/image';
 export function LoginInfoPanel() {
   return (
     <div className="relative hidden w-full overflow-hidden bg-primary p-8 text-white md:flex md:w-1/2 flex-col justify-center items-center">
+        <div className="absolute top-4 left-4 z-20">
+            <Image 
+                src="/logo-kemenag.png"
+                alt="Logo Kementerian Agama"
+                width={50}
+                height={50}
+            />
+        </div>
         <div className="absolute -top-16 -left-16 w-64 h-64 bg-primary/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
         <div className="absolute -bottom-24 -right-2 w-72 h-72 bg-accent/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         <div className="w-full max-w-sm z-10">
              <Image
-              src="https://storage.googleapis.com/project-hackathon-2024-public/kua-staff-illustration.png"
+              src="/ketuakua2.png"
               alt="Ilustrasi Petugas KUA"
-              width={400}
-              height={500}
+              width={250}
+              height={450}
               className="rounded-lg object-contain"
               priority
             />
